9e68b80b7aad67bf64fd13c2852c052f26eea6d3
RyJoMi <tup88794@temple.edu>
2025-04-03 15:10:11 -0400

    idk

--- script.js
+++ script.js
@@ -2,17 +2,24 @@ const thisAudio = new AudioContext();
 
 let root = 440;
 let maj7 = [1, 1.25, 1.5, 1.875];
-let quality = [root * maj7[0], root * maj7[1], root * maj7[2], root * maj7[3]];
+let dom7 = [1, 1.25, 1.5, 1.8];
+let min7 = [1, 1.2, 1.5, 1.8];
+let quality = [
+  root * `${document.getElementById("chor1Qual").value}`[0],
+  root * `${document.getElementById("chor1Qual").value}`[1],
+  root * `${document.getElementById("chor1Qual").value}`[2],
+  root * `${document.getElementById("chor1Qual").value}`[3],
+];
 
 const playChord1 = function () {
   //saves current time for reference
   let now = thisAudio.currentTime;
 
   //create osc
-  myOsc1 = thisAudio.createOscillator();
-  myOsc2 = thisAudio.createOscillator();
-  myOsc3 = thisAudio.createOscillator();
-  myOsc4 = thisAudio.createOscillator();
+  let myOsc1 = thisAudio.createOscillator();
+  let myOsc2 = thisAudio.createOscillator();
+  let myOsc3 = thisAudio.createOscillator();
+  let myOsc4 = thisAudio.createOscillator();
 
   //frequencies
   myOsc1.frequency.value = quality[0];
@@ -25,10 +32,10 @@ const playChord1 = function () {
   myOsc3.type = "triangle";
   myOsc4.type = "triangle";
   //connect
-  myOsc1.connect(output);
-  myOsc2.connect(output);
-  myOsc3.connect(output);
-  myOsc4.connect(output);
+  myOsc1.connect(thisAudio.destination);
+  myOsc2.connect(thisAudio.destination);
+  myOsc3.connect(thisAudio.destination);
+  myOsc4.connect(thisAudio.destination);
   //start
   myOsc1.start();
   myOsc2.start();
@@ -36,9 +43,6 @@ const playChord1 = function () {
   myOsc4.start();
 };
 
-const log = function () {
-  document.getElementById("log").textContent = root;
-};
-
 let playBut = document.getElementById("play");
-playBut.addEventListener("click", playChord1, run, log);
+
+playBut.addEventListener("click", playChord1);
